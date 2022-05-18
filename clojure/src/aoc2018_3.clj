48947ceee680d3ce3eca35c24843c1038568d214
Ma-rk <kim.mark.dev@gmail.com>
2022-05-18 14:24:33 +0900

    리팩토링 3

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -2,10 +2,9 @@
   (:require [clojure.set :as set]
             [clojure.string :as str]))
 
-
-(defn get-day3-input-as-list
+(defn input-txt->line-vector
   "
-   입력받은 경로의 파일을 읽어 파일의 내용을 반환
+   입력받은 경로의 파일을 읽어 파일의 내용을 반환(vector)
 
    input sample: src/input/aoc2018_3_input.txt
    output sample: [\"#1 @ 1,3: 4x4\", ... \"#3 @ 5,5: 2x2\"]
@@ -38,133 +37,143 @@
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
 
-
-(defn get-points-of-upper-line
-  "
-   사각형의 상단 라인을 구성하는 점의 번호들을 리턴
-   
-   n: 전체 febric의 가로 폭
-   left: 상단 라인의 왼쪽 시작점
-   top: 상단 라인과 febric의 최상부의 거리
-   width: 상단 라인의 가로 폭
-
-   output: #{3 4 5 6}
-   "
-  [n
-   left
-   top
-   width]
-  (let [left-top-angle (+ (* (- top 1) n) left)
-        right-top-angle (+ left-top-angle (- width 1))]
-    (set (range left-top-angle
-                (+ right-top-angle 1)))))
-
-
-(defn get-points-of-box
-  "
-   사각형의 상단 라인을 구성하는 점의 번호들을 입력받아 사각형 전체를 구헝하는 모든 점의 번호를 리턴
-   
-   top-line-points: 사각형의 상단 라인을 구성하는 점의 번호들
-   n: 전체 febric의 가로 폭
-   hight: 사각형의 높이
-   
-   output: (14 11 12 13 21 18 19 20 28 25 26 27)
-   "
-  [top-line-points
-   n
-   hight]
-  (loop [result-list '()
-         h (- hight 1)]
-    (if (< h 0)
-      result-list
-      (recur  (into result-list (map #(+ (* n h) %) top-line-points))
-              (- h 1)))))
-
 (defn line->box
   "
-   문제의 input의 한 line을 입력받아 사각형의 4개의 점의 번호를 가진 set을 리턴
+   문제의 input의 한 line을 입력받아 box로 변환
 
-   line: #1 @ 1,3: 4x4
+   input:
+   - line: 문제의 input text 파일의 한 line
+   -- example value: \"#1388 @ 962,907: 17x16\"
    
-   output:{:left 1, :top 3, :width 4, :hight 4}
+   output:
+   - (anonymous): 사각형의 속성을 나타내는 map
+   -- example value: {:id 1388 :left 962, :top 907, :width 17, :hight 16}
    "
   [line]
   (let [splitted (str/split (clojure.string/replace line ":" "") #" ")
-        left-top-point (nth splitted 2)
-        left (Integer/parseInt (first (str/split left-top-point #",")))
-        top (Integer/parseInt (last (str/split left-top-point #",")))
+        id (clojure.string/replace (nth splitted 0) "#" "")
+        left-top-pixel (nth splitted 2)
+        left (Integer/parseInt (first (str/split left-top-pixel #",")))
+        top (Integer/parseInt (last (str/split left-top-pixel #",")))
         area (nth splitted 3)
         width (Integer/parseInt (first (str/split area #"x")))
         hight (Integer/parseInt (last (str/split area #"x")))]
-    {:left left :top top :width width :hight hight}))
+    {:id id :left left :top top :width width :hight hight}))
 
-(defn line-list->box-list
+
+(defn line-vector->box-seq
   "
-   문제의 input의 전체를 입력받아 사각형의 4개의 점의 번호를 가진 set의 리스트로 변환하여 리턴
-   "
-  [line-list]
-  (map line->box line-list))
+   input의 모든 line을 입력받아 box의 seq로 변환
 
-(defn get-biggest-width
+   input:
+   - line-vector: 문제의 input text 파일의 각 line으로 구성된 vector
+   -- example value: [\"#1388 @ 962,907: 17x16\" \"#1388 @ 962,907: 17x16\" ...]
+   
+   output:
+   - (anonymous): box로 구성된 seq
+   -- example value: ({:id 1388 :left 962, :top 907, :width 17, :hight 16} ...)
+   "
+  [line-vector]
+  (map line->box line-vector))
+
+
+(defn get-febric-width
   "
    input 전부를 분석해서 전체 febric의 가로 폭을 계산(모든 라인의 우측 끝점 중 가장 큰 수를 리턴)
 
-   line: #1 @ 1,3: 4x4
+   input:
+   - box-seq:  box로 구성된 seq
+   -- example value: ({:id 1388 :left 962, :top 907, :width 17, :hight 16} ...)
    
-   output: 435
+   output:
+   - fegric-width: 전체 febric의 가로 폭
+   -- example value: 746
    "
-  [given-box-list]
-  (loop [loop-box-list given-box-list
-         biggest-width 0]
+  [box-seq]
+  (loop [loop-box-list box-seq
+         fegric-width 0]
     (if (empty? loop-box-list)
-      biggest-width
+      fegric-width
       (let [left (:left (first loop-box-list))
             width (:width (first loop-box-list))
-            most-right-point (- (+ left width)  1)
-            biggest-width (max biggest-width most-right-point)]
+            most-right-pixel (- (+ left width)  1)
+            fegric-width (max fegric-width most-right-pixel)]
         (recur (rest loop-box-list)
-               biggest-width)))))
+               fegric-width)))))
 
-(defn execute
-  [every-box-list]
-  (let [biggest-width (get-biggest-width every-box-list)]
-    (loop [every-box every-box-list
-           inter-sector #{}
-           whole-sector #{}]
-      (if (empty? every-box)
-        [inter-sector whole-sector]
-        (let [box (first every-box)
-              left (:left box)
-              top (:top box)
-              width (:width box)
-              hight (:hight box)
-              top-line-points (get-points-of-upper-line biggest-width left top width)
-              points-of-box (set (get-points-of-box top-line-points biggest-width hight))]
-          (recur (rest every-box)
-                 (set/union inter-sector (set/intersection points-of-box whole-sector))
-                 (set/union whole-sector points-of-box)))))))
+(defn box->box-pixels
+  "
+   전체 febric의 가로 폭과 box를 입력받아 box를 구성하는 모든 pixel의 번호를 리턴
+
+   input:
+   - febric-width: 전체 febric의 가로 폭
+   -- example value: 746
+   - box: box
+   -- example value: {:id 1388 :left 962, :top 907, :width 17, :hight 16}
+   
+   output:
+   - box-pixels: 하나의 box를 구성하는 pixel의 번호들
+   -- example value: (4 5 6 10 11 12)
+   "
+  [febric-width box]
+  (let [left-top-angle (+ (* (- (:top box) 1) febric-width) (:left box))
+        right-top-angle (+ left-top-angle (- (:width box) 1))
+        top-line-pixels (range left-top-angle (+ right-top-angle 1))
+        hight (:hight box)]
+    (loop [box-pixels '()
+           h (- hight 1)]
+      (if (< h 0)
+        box-pixels
+        (recur  (into box-pixels (map #(+ (* febric-width h) %) top-line-pixels))
+                (- h 1))))))
+
+(defn box-seq->box-pixels-seq
+  "
+   box의 seq를 입력받아 box를 구성하는 모든 pixel의 번호로 구성된 seq의 seq를 리턴
+
+   input:
+   - box-seq: box의 seq
+   -- example value: ({:id 1388 :left 962, :top 907, :width 17, :hight 16} ...)
+   
+   output:
+   - (anonymous): box를 구성하는 모든 pixel의 번호로 구성된 seq의 seq
+   -- example value: ((1 2 3) (4 5 6) ...)
+   "
+  [box-seq]
+  (let [febric-width (get-febric-width box-seq)]
+    (map (fn [box] (box->box-pixels febric-width box)) box-seq)))
+
+(defn box-pixels-seq->inter-pixel-set
+  "
+   모든 box의 pixel들을 입력받아 2개 이상의 box에 속한 pixel 번호를 set에 담아 리턴
+
+   input
+   - box-pixels-seq: box를 구성하는 모든 pixel의 번호로 구성된 seq의 seq
+   -- example value: ((1 2 3) (4 5 6) ...)
+   output: 
+   - inter-pixel: 서로 다른 box간에 겹치는 pixel의 번호가 담긴 set
+   -- example value: #(15 16 21 22)
+   "
+  [box-pixels-seq]
+  (loop [whole-pixel #{}
+         inter-pixel #{}
+         box-pixels-loop box-pixels-seq]
+    (if (empty? box-pixels-loop)
+      inter-pixel
+      (recur (clojure.set/union whole-pixel (set (first box-pixels-loop)))
+             (clojure.set/union inter-pixel (clojure.set/intersection whole-pixel (set (first box-pixels-loop))))
+             (rest box-pixels-loop)))))
 
 (comment
-  (-> "src/input/aoc2018_3_input.txt"
-      (get-day3-input-as-list)
-      (line-list->box-list)
-      (execute)
-      (first)
-      (count))
-
   (->
-   (get-points-of-upper-line 7 4 2 4))
+   "src/input/aoc2018_3_input.txt"
+   (input-txt->line-vector)
+   (line-vector->box-seq)
+   (box-seq->box-pixels-seq)
+   (box-pixels-seq->inter-pixel-set)
+   (count))
 
-  (->
-   (get-points-of-upper-line 7 4 2 4)
-   (get-points-of-box 7 3)
-   (set))
-
-  (->
-   (line->box "#1 @ 287,428: 27x20"))
-  (-> "src/input/aoc2018_3_input.txt"
-      (get-day3-input-as-list)
-      (line-list->box-list)))
+  (+ 1 1))
 
 ;; 파트 2
 ;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
