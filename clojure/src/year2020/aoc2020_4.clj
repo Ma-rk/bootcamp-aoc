acaa3c954c63da80b6fee5a5443b97cec24a7f8e
Ma-rk <kim.mark.dev@gmail.com>
2022-05-30 19:20:11 +0900

    코드 개선

--- clojure/src/year2020/aoc2020_4.clj
+++ clojure/src/year2020/aoc2020_4.clj
@@ -1,8 +1,7 @@
 (ns aoc2020_4
-  (:require [clojure.string :as str])
-  (:require [clojure.pprint :as pp])
-  (:require [common :refer [input-txt->line]]))
-
+  (:require [clojure.string :as str]
+            [common :refer [input-txt->line]]
+            [clojure.spec.alpha :as s]))
 
 ;; part 1
 ;; Count the number of valid passports - those that have all required fields. Treat cid as optional.
@@ -15,52 +14,43 @@
    output:   {\"hcl\" \"0cacc5\", \"byr\" \"1944\", \"iyr\" \"2028\", \"eyr\" \"2024\", \"hgt\" \"163in\", \"pid\" \"74169361\", \"ecl\" \"dne\"}
    "
   [key-value-list]
-  (loop [key-value-loop key-value-list
-         passport-map {}]
-    (if (empty? key-value-loop) passport-map
-        (let [key (first (str/split (first key-value-loop) #":"))
-              val (last (str/split (first key-value-loop) #":"))]
-          (recur (rest key-value-loop)
-                 (assoc passport-map key val))))))
+  (->> key-value-list
+       (map (fn [key-value] (let [[key value] (str/split key-value #":")] [(keyword key) value])))
+       (into {})))
 
-(defn convert-passport-line-to-passport-map
+
+(defn input-line->passport-maps
   "
    input 데이터 전체를 변환하여 passport map의 coll을 반환
 
    input: \"eyr:2033\r\nhgt:177cm pid:173cm\r\necl:utc byr:2029 hcl:#efcc98 iyr:2023\r\n\r\npid:337605855...\"
    output: '( {\"ecl\" \"hzl\", \"hcl\" \"#a97842\", \"hgt\" \"157cm\", \"eyr\" \"2025\", \"iyr\" \"2015\", \"byr\" \"1978\", \"pid\" \"579525362\"} {\"ecl\" \"hzl\", \"hcl\" \"#a97842\" ...} ... )
    "
-  [passport-lines]
-  (let [a (-> passport-lines
-              (str/replace #" " "\r\n")
-              (str/split #"\r\n\r\n"))]
-    (->> a
+  [passport-line]
+  (let [processing-line (-> passport-line
+                            (str/replace #" " "\r\n")
+                            (str/split #"\r\n\r\n"))]
+    (->> processing-line
          (map (fn [line] (str/split line #"\r\n")))
          (map (fn [line] (gen-passport-map line))))))
 
-(defn filter-valid-passport-via-field
+(defn passport-has-valid-field
   "
    필수 필드를 모두 갖고 있으면 true
    하나라도 없으면 false
 
-   input: {\"ecl\" \"hzl\", \"hcl\" \"#a97842\", \"hgt\" \"157cm\", \"eyr\" \"2025\", \"iyr\" \"2015\", \"byr\" \"1978\", \"pid\" \"579525362\"}
-   output: false
+   input: [{\"ecl\" \"hzl\", \"hcl\" \"#a97842\", \"hgt\" \"157cm\", \"eyr\" \"2025\", \"iyr\" \"2015\", \"byr\" \"1978\", \"pid\" \"579525362\"} ... ]
+   output: 필수 필드를 모두 갖고 있는 map으로 구성된 컬렉션
    "
-  [passports]
-  (filter (fn [passport] (and (contains? passport "byr")
-                              (contains? passport "iyr")
-                              (contains? passport "eyr")
-                              (contains? passport "hgt")
-                              (contains? passport "hcl")
-                              (contains? passport "ecl")
-                              (contains? passport "pid"))) passports))
+  [passport]
+  (clojure.set/subset? #{:byr :iyr :eyr :hgt :hcl :ecl :pid} (set (keys passport))))
 
 (comment
-  (-> "src/year2020/aoc2020_4.txt"
-      input-txt->line
-      convert-passport-line-to-passport-map
-      filter-valid-passport-via-field
-      count))
+  (->> "src/year2020/aoc2020_4.txt"
+       input-txt->line
+       input-line->passport-maps
+       (filter passport-has-valid-field?)
+       count))
 
 
 
