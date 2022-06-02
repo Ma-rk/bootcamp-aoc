c330625acc238440cde0dd511190168cb0cbeec9
Ma-rk <kim.mark.dev@gmail.com>
2022-06-02 11:41:08 +0900

    약어를 원래 단어로 되돌림

--- clojure/src/aoc2018_6.clj
+++ clojure/src/aoc2018_6.clj
@@ -65,22 +65,22 @@
   "좌표계의 사이즈를 이용해 전체 좌표계를 나타내는 map으로 구성된 collection을 생성
 
   input ex) {:max-x 353, :max-y 357}
-  output ex) '({:gd-x 0, :gd-y 0} {:gd-x 1, :gd-y 0} {:gd-x 2, :gd-y 0} {:gd-x 3, :gd-y 0} {:gd-x 4, :gd-y 0} {:gd-x 5, :gd-y 0} {:gd-x 6, :gd-y 0} {:gd-x 7, :gd-y 0} {:gd-x 8, :gd-y 0} {:gd-x 0, :gd-y 1} {:gd-x 1, :gd-y 1} ... )"
+  output ex) '({:grid-x 0, :grid-y 0} {:grid-x 1, :grid-y 0} {:grid-x 2, :grid-y 0} {:grid-x 3, :grid-y 0} {:grid-x 4, :grid-y 0} {:grid-x 5, :grid-y 0} {:grid-x 6, :grid-y 0} {:grid-x 7, :grid-y 0} {:grid-x 8, :grid-y 0} {:grid-x 0, :grid-y 1} {:grid-x 1, :grid-y 1} ... )"
   [board-size]
   (for [y (range (inc (board-size :max-y)))
         x (range (inc (board-size :max-x)))]
-    {:gd-x x :gd-y y}))
+    {:grid-x x :grid-y y}))
 
 (defn get-dist
   "두 점(하나의 grid와 하나의 point) 사이의 멘하탄 거리를 측정
 
   input ex)
-  - grid: {:gd-x 8, :gd-y 3}
+  - grid: {:grid-x 8, :grid-y 3}
   - point {:pt-x 306, :pt-y 102}
   output ex) 397"
   [grid point]
-  (let [diff-x (Math/abs (- (grid :gd-x) (point :pt-x)))
-        diff-y (Math/abs (- (grid :gd-y) (point :pt-y)))]
+  (let [diff-x (Math/abs (- (grid :grid-x) (point :pt-x)))
+        diff-y (Math/abs (- (grid :grid-y) (point :pt-y)))]
     (+ diff-x diff-y)))
 
 (defn get-nearest-point-from-grid
@@ -88,7 +88,7 @@
   가장 가까운 point가 하나보다 많으면 :tied를 반환
 
   input ex)
-  - grid: {:gd-x 8, :gd-y 3}
+  - grid: {:grid-x 8, :grid-y 3}
   - points: '({:pt-x 78, :pt-y 335} {:pt-x 74, :pt-y 309} {:pt-x 277, :pt-y 44} ...)  
   output ex) {:pt-x 306, :pt-y 102} or :tied"
   [grid points]
@@ -104,23 +104,23 @@
   "모든 grid에 대하여 각각 가장 가까운 point를 표시
   가장 가까운 point가 하나가 아니면 :tied를 표시
 
-  input ex) '({:gd-x 0, :gd-y 0} {:gd-x 1, :gd-y 0} {:gd-x 2, :gd-y 0} {:gd-x 3, :gd-y 0} {:gd-x 4, :gd-y 0} {:gd-x 5, :gd-y 0} {:gd-x 6, :gd-y 0} {:gd-x 7, :gd-y 0} {:gd-x 8, :gd-y 0} {:gd-x 0, :gd-y 1} {:gd-x 1, :gd-y 1} ... )
-  output ex) '{:gd-x 4, :gd-y 0, :nearest-pt {:pt-x 1, :pt-y 1}} {:gd-x 5, :gd-y 0, :nearest-pt :tied} {:gd-x 6, :gd-y 0, :nearest-pt {:pt-x 8, :pt-y 3}} ...)"
+  input ex) '({:grid-x 0, :grid-y 0} {:grid-x 1, :grid-y 0} {:grid-x 2, :grid-y 0} {:grid-x 3, :grid-y 0} {:grid-x 4, :grid-y 0} {:grid-x 5, :grid-y 0} {:grid-x 6, :grid-y 0} {:grid-x 7, :grid-y 0} {:grid-x 8, :grid-y 0} {:grid-x 0, :grid-y 1} {:grid-x 1, :grid-y 1} ... )
+  output ex) '{:grid-x 4, :grid-y 0, :nearest-pt {:pt-x 1, :pt-y 1}} {:grid-x 5, :grid-y 0, :nearest-pt :tied} {:grid-x 6, :grid-y 0, :nearest-pt {:pt-x 8, :pt-y 3}} ...)"
   [grids points]
   (map (fn [grid] (assoc grid :nearest-pt (get-nearest-point-from-grid grid points))) grids))
 
 (defn collect-infitite-points
   "영역이 무한히 확장되는 points를 찾음
   
-  input ex) '{:gd-x 4, :gd-y 0, :nearest-pt {:pt-x 1, :pt-y 1}} {:gd-x 5, :gd-y 0, :nearest-pt :tied} {:gd-x 6, :gd-y 0, :nearest-pt {:pt-x 8, :pt-y 3}} ...) 
+  input ex) '{:grid-x 4, :grid-y 0, :nearest-pt {:pt-x 1, :pt-y 1}} {:grid-x 5, :grid-y 0, :nearest-pt :tied} {:grid-x 6, :grid-y 0, :nearest-pt {:pt-x 8, :pt-y 3}} ...) 
   output ex) '({:pt-x 1, :pt-y 1} {:pt-x 8, :pt-y 3} {:pt-x 1, :pt-y 6} {:pt-x 8, :pt-y 9})"
   [marked-grids]
-  (let [max-x (apply max (map :gd-x marked-grids))
-        max-y (apply max (map :gd-y marked-grids))
-        edge-reached-grids (filter (fn [grid] (or (= (:gd-x grid) max-x)
-                                                  (= (:gd-y grid) max-y)
-                                                  (= (:gd-x grid) 0)
-                                                  (= (:gd-y grid) 0))) marked-grids)
+  (let [max-x (apply max (map :grid-x marked-grids))
+        max-y (apply max (map :grid-y marked-grids))
+        edge-reached-grids (filter (fn [grid] (or (= (:grid-x grid) max-x)
+                                                  (= (:grid-y grid) max-y)
+                                                  (= (:grid-x grid) 0)
+                                                  (= (:grid-y grid) 0))) marked-grids)
         infitite-points (map :nearest-pt edge-reached-grids)
         distinct-points (distinct infitite-points)]
     (filter (fn [point] (and (not= :tied point) (not= :point point))) distinct-points)))
@@ -133,11 +133,11 @@
   [points infitite-points]
   (st/difference (set points) (set infitite-points)))
 
-(defn get-count-of-most-great-finite-points
+(defn get-count-of-each-finite-points
   "영역이 유한한 point 중 가장 영역이 넓은 point의 넓이를 반환
   
   input ex)
-  - marked-grids: '{:gd-x 4, :gd-y 0, :nearest-pt {:pt-x 1, :pt-y 1}} {:gd-x 5, :gd-y 0, :nearest-pt :tied} {:gd-x 6, :gd-y 0, :nearest-pt {:pt-x 8, :pt-y 3}} ...) 
+  - marked-grids: '{:grid-x 4, :grid-y 0, :nearest-pt {:pt-x 1, :pt-y 1}} {:grid-x 5, :grid-y 0, :nearest-pt :tied} {:grid-x 6, :grid-y 0, :nearest-pt {:pt-x 8, :pt-y 3}} ...) 
   - finite-points: #{{:pt-x 3, :pt-y 4} {:pt-x 5, :pt-y 5}} 
   output ex) 17"
   [marked-grids finite-points]
@@ -155,8 +155,9 @@
         count-of-finite-points (->> marked-grids
                                     collect-infitite-points
                                     (get-finite-point points)
-                                    (get-count-of-most-great-finite-points marked-grids))]
+                                    (get-count-of-each-finite-points marked-grids))]
     (val (first (sort-by val > count-of-finite-points))))
+  (class [{:pt-x 5, :pt-y 5} 17]))
 ;; 파트 2
 ;; 안전(safe) 한 지역은 근원지'들'로부터의 맨하탄거리(Manhattan distance, 격자를 상하좌우로만 움직일때의 최단 거리)의 '합'이 N 미만인 지역임.
 
@@ -184,7 +185,7 @@
 (defn get-dist-from-points
   "하나의 drid를 기준으로 모든 point와의 맨하탄 거리의 합을 구함
 
-  input ex) {:gd-x 8, :gd-y 3}
+  input ex) {:grid-x 8, :grid-y 3}
   output ex) '({:pt-x 78, :pt-y 335} {:pt-x 74, :pt-y 309} {:pt-x 277, :pt-y 44} ...)"
   [grid points]
   (reduce + (map (fn [point] (get-dist grid point)) points)))
@@ -192,7 +193,7 @@
 (defn mark-distance-on-grid
   "grid와 모든 point들간의 거리의 합을 구해 grid에 표시
 
-  input ex) '({:gd-x 0, :gd-y 0} {:gd-x 1, :gd-y 0} {:gd-x 2, :gd-y 0} {:gd-x 3, :gd-y 0} {:gd-x 4, :gd-y 0} {:gd-x 5, :gd-y 0} {:gd-x 6, :gd-y 0} {:gd-x 7, :gd-y 0} {:gd-x 8, :gd-y 0} {:gd-x 0, :gd-y 1} {:gd-x 1, :gd-y 1} ... )
+  input ex) '({:grid-x 0, :grid-y 0} {:grid-x 1, :grid-y 0} {:grid-x 2, :grid-y 0} {:grid-x 3, :grid-y 0} {:grid-x 4, :grid-y 0} {:grid-x 5, :grid-y 0} {:grid-x 6, :grid-y 0} {:grid-x 7, :grid-y 0} {:grid-x 8, :grid-y 0} {:grid-x 0, :grid-y 1} {:grid-x 1, :grid-y 1} ... )
   output ex) "
   [grids points]
   (map (fn [grid] (assoc grid :dist (get-dist-from-points grid points)))  grids))
@@ -200,7 +201,7 @@
 (defn get-count-of-dist-under-10000
   "grids 중에서 다른 point들과의 거리의 합이 10000 미만인 drid의 개수를 구함
 
-  input ex) ({:gd-x 0, :gd-y 0, :dist 20335} {:gd-x 1, :gd-y 0, :dist 20285} {:gd-x 2, :gd-y 0, :dist 20235} {:gd-x 3, :gd-y 0, :dist 20185} ...)
+  input ex) ({:grid-x 0, :grid-y 0, :dist 20335} {:grid-x 1, :grid-y 0, :dist 20285} {:grid-x 2, :grid-y 0, :dist 20235} {:grid-x 3, :grid-y 0, :dist 20185} ...)
   output ex) 15"
   [dist-appended-grids]
   (count (filter (fn [grid] (< (:dist grid) 10000)) dist-appended-grids)))
