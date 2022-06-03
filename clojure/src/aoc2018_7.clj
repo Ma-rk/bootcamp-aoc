63409a2adbf48dde78073ec8aa2c2df51105f488
Ma-rk <kim.mark.dev@gmail.com>
2022-06-03 11:10:35 +0900

    day 7 파일 추가

---
+++ clojure/src/aoc2018_7.clj
@@ -0,0 +1,10 @@
+(ns aoc2018_7)
+
+;; part 1
+;; In what order should the steps in your instructions be completed?
+
+
+
+;; part 2
+;; With 5 workers and the 60+ second step durations described above,
+;; how long will it take to complete all of the steps?
