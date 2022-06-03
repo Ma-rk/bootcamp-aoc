cade185f72aeb4063e67171a2c9ac8dd92851a81
Ma-rk <kim.mark.dev@gmail.com>
2022-06-03 15:42:46 +0900

    스레딩 매크로 적용

--- clojure/src/aoc2018_7.clj
+++ clojure/src/aoc2018_7.clj
@@ -17,7 +17,7 @@
                            (str/split #" must be finished before step "))))
        (map (fn [splited-line]  {:from (first splited-line) :to (last splited-line)}))))
 
-(defn find-one-entry-step
+(defn find-next-entry-step
   "instructions에 남은 step들 중 from에는 있지만 to에는 없는 step을 찾아
    그중 알파벳 순서가 가장 빠른 step을 반환한다.
    찾은 step을 from으로 갖고 있는 instruction들은 instructions에서 제거하고 남은 것을 반환한다.
@@ -36,13 +36,18 @@
      rest-instructions]))
 
 (defn get-completed-instructions
-  "instructions를 입력받아 올바른 조립 순서를 뜻하는 string을 만들어 반환한다. (rom instructions만 모으기 때문에 마지막 한 step은 누락된다)
+  "instructions를 입력받아 올바른 조립 순서를 뜻하는 string을 만들어 반환한다.
   
    input ex) '({:from \"B\", :to \"G\"} {:from \"G\", :to \"J\"} {:from \"J\", :to \"F\"} {:from \"U\", :to \"Z\"} ... )
-   output ex) CABDF
+   output ex) CABDFE
    "
   [instructions]
-  (reduce str (map #(first %) (take-while #(not (nil? (first %))) (iterate find-one-entry-step ["" instructions])))))
+  (->> ["" instructions]
+       (iterate find-next-entry-step)
+       (take-while #(not (nil? (first %))))
+       (map #(first %))
+       (reduce str)
+       (append-last-letter instructions)))
 
 
 (defn append-last-letter
@@ -55,7 +60,7 @@
    - instructions: '({:from \"B\", :to \"G\"} {:from \"G\", :to \"J\"} {:from \"J\", :to \"F\"} {:from \"U\", :to \"Z\"} ... )
    output ex) CABDFE
    "
-  [completed-instructions instructions]
+  [instructions completed-instructions]
   (let [completed-letters (set (map str (set completed-instructions)))
         to-letters (set (map :to instructions))
         last-letter (set/difference to-letters completed-letters)]
@@ -63,13 +68,10 @@
 
 
 (comment
-  (let [refined-instructions (-> "src/input/aoc2018_7_input.txt"
-                                 input-txt->line-vector
-                                 refine-instructions)
-        completed-instructions (get-completed-instructions refined-instructions)]
-    (append-last-letter completed-instructions refined-instructions)))
-
-
+  (-> "src/input/aoc2018_7_input.txt"
+      input-txt->line-vector
+      refine-instructions
+      get-completed-instructions))
 ;; part 2
 ;; With 5 workers and the 60+ second step durations described above,
 ;; how long will it take to complete all of the steps?
